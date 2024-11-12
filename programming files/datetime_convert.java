6c5e3359b03a439ddae524b73b4a2d2755754c25
lVagtBendoski <157419244+lVagtBendoski@users.noreply.github.com>
2024-11-12 13:55:39 -0500

    broke it

--- programming files/datetime_convert.java
+++ programming files/datetime_convert.java
@@ -4,11 +4,19 @@ import java.time.format.DateTimeFormatter;
 
 public class datetime_convert {
     public static void main(String[] args) {
-        String dateStr = "2022-03-17 10:45:30"; // current date
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"); // formats date to readible format
-        LocalDateTime dateObj = LocalDateTime.parse(dateStr, formatter); // creates a date object to be used
-        String formattedDate = dateObj.format(DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss")); // reformats date to month day year format
-
-        System.out.println(formattedDate); // prints out date
+        String correctDateStr = "2022-03-17 10:45:30"; // current date
+        
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter date and time");
+        String dateStr = myObj.nextLine();  // Read user input
+        if(dateStr != correctDateStr){
+            system.out.println("bad");
+        }else{
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"); // formats date to readible format
+            LocalDateTime dateObj = LocalDateTime.parse(dateStr, formatter); // creates a date object to be used
+            String formattedDate = dateObj.format(DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss")); // reformats date to month day year format
+    
+            System.out.println(formattedDate); // prints out date
+        }
     }
 }
