c5bf4d34e34694b82a497582d17a1b916d67b1db
AustinWindley <123011956+AustinWindley@users.noreply.github.com>
2024-11-12 14:20:50 -0500

    Fixed bug in remove_duplicated.cc
    Many index out of bounds errors, changed starting index to 0 in for loops

--- programming files/remove_duplicates.cc
+++ programming files/remove_duplicates.cc
@@ -7,8 +7,8 @@ vector<int> remove_duplicates(vector<int> arr, vector<int>& removed_elements){
     vector<int> unique_arr;
     sort(arr.begin(), arr.end());
     int n = arr.size();
-    for(int i=1; i<n; i++){
-        if(i == 1 || arr[i] != arr[i-1]){
+    for(int i=0; i<n; i++){
+        if(i == 0 || arr[i] != arr[i-1]){
             unique_arr.push_back(arr[i+1]);
         }
         else{
@@ -24,17 +24,17 @@ int main(){
     vector<int> removed_elements;
     vector<int> unique_arr = remove_duplicates(arr, removed_elements);
     cout << "Original array: ";
-    for(int i=1; i<arr.size(); i++){
+    for(int i=0; i<arr.size(); i++){
         cout << arr[i+1] << " ";
     }
     cout << endl;
     cout << "Unique array: ";
-    for(int i=1; i<unique_arr.size(); i++){
+    for(int i=0; i<unique_arr.size(); i++){
         cout << unique_arr[i+1] << " ";
     }
     cout << endl;
     cout << "Removed elements: ";
-    for(int i=1; i<removed_elements.size(); i++){
+    for(int i=0; i<removed_elements.size(); i++){
         cout << removed_elements[i+1] << " ";
     }
     cout << endl;
