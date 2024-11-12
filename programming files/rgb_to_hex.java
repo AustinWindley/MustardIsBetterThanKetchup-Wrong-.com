280617277e9daefe834748a14c2a560ccba0af6c
lVagtBendoski <157419244+lVagtBendoski@users.noreply.github.com>
2024-11-12 14:20:18 -0500

    fixed always #000000 bug

--- programming files/rgb_to_hex.java
+++ programming files/rgb_to_hex.java
@@ -10,9 +10,9 @@ public class rgb_to_hex {
     }
 // this is the function that actually converts the 3 ints to a string with hex format
     public static String rgbToHex(int r, int g, int b) {
-        r = Math.min(255, Math.min(0, r));
-        g = Math.min(255, Math.min(0, g));
-        b = Math.min(255, Math.min(0, b));
+        r = Math.min(255, Math.max(0, r));
+        g = Math.min(255, Math.max(0, g));
+        b = Math.min(255, Math.max(0, b));
         return String.format("%02X%02X%02X", r, g, b);
     }
 }
